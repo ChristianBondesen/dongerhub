4ddbff27a0a0fde20157b92e8271956744e6b40b
Simon Bundgaard-Egeberg <au515936@uni.au.dk>
2018-01-11 12:06:19 +0100

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { MaterialModule } from './material.module';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AppRoutingModule } from './router.module';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, MaterialModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent
+  ],
+  imports: [BrowserModule, MaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/router.module.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+
+const routes: Routes = [{ path: '', component: HomepageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
