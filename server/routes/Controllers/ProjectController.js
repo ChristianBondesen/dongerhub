a0f330560c2285288f41658b5b638ca092c8b0c3
Simon Bundgaard-Egeberg <au515936@uni.au.dk>
2018-02-08 13:02:22 +0100

    eslint

--- server/routes/Controllers/ProjectController.js
+++ server/routes/Controllers/ProjectController.js
@@ -2,19 +2,19 @@
 const express = require('express');
 const ProjectRouter = express.Router();
 const bodyParser = require('body-parser');
-ProjectRouter.use(bodyParser.urlencoded({ extended: true }));
+ProjectRouter.use(bodyParser.urlencoded({
+  extended: true
+}));
 ProjectRouter.use(bodyParser.json());
 const Project = require('../DatabaseModels/project.js');
-const config = require('../config.js');
 
 
 // GetAll Projects
 ProjectRouter.get('/', (request, result) => {
   Project.find({}, (err, res) => {
-    if(err) {
+    if (err) {
       result.status(400).send(err);
-    }
-    else {
+    } else {
       result.status(200).send(res);
     }
   });
@@ -22,39 +22,35 @@ ProjectRouter.get('/', (request, result) => {
 
 // GetById Project:id
 ProjectRouter.get('/:id', (request, result) => {
-    Project.findById(id, (err, prjct) => {
-      if(err) {
-        result.status(400).send(err);
-      }
-      else {
-        result.status(200).send(prjct);
-      }
-    });
+  Project.findById(id, (err, prjct) => {
+    if (err) {
+      result.status(400).send(err);
+    } else {
+      result.status(200).send(prjct);
+    }
+  });
 });
 
 // Post Project
 ProjectRouter.post('/', (request, result) => {
   Project.create({
-    projectName: request.body.projectName,
-    users: request.body.users,
-    hoursSpent: request.body.hoursSpent
-  }),
-  (err, project) => {
-    if(err) {
-      result.status(400).json({
-        project: null,
-        succes: false
-      });
-    }
-    else {
-      result.status(200).json({
-        project: project,
-        succes: true,
-      });
-    }
-  };
+      projectName: request.body.projectName,
+      users: request.body.users,
+      hoursSpent: request.body.hoursSpent
+    }),
+    (err, project) => {
+      if (err) {
+        result.status(400).json({
+          project: null,
+          succes: false
+        });
+      } else {
+        result.status(200).json({
+          project: project,
+          succes: true,
+        });
+      }
+    };
 });
 
 module.exports = ProjectRouter;
-
-
