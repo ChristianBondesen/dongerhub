ad575c99924b0094fda313a407199854a388da0e
Simmetopia <egeberg.simon@gmail.com>
2018-02-08 11:41:40 +0100

    Added validation for register module

--- src/app/pages/user-login/user-register/user-register.component.ts
+++ src/app/pages/user-login/user-register/user-register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserLoginService } from '../user-login.service';
 
 @Component({
@@ -12,13 +12,31 @@ export class UserRegisterComponent implements OnInit {
   constructor(private fb: FormBuilder, private service: UserLoginService) {}
 
   ngOnInit() {
-    this.newUser = this.fb.group({
-      name: [''],
-      username: [''],
-      password: [''],
-      repeatPassword: [''],
-      email: ['']
-    });
+    this.newUser = this.fb.group(
+      {
+        name: ['', Validators.required],
+        username: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        repeatPassword: ['', [Validators.required, Validators.minLength(6)]],
+        email: ['', [Validators.required, Validators.email]]
+      },
+      {
+        validator: this.formGroupValidationFunction(
+          'password',
+          'repeatPassword'
+        )
+      }
+    );
+  }
+  formGroupValidationFunction(a: string, b: string) {
+    return (formGroup: FormGroup): void => {
+      const field1 = formGroup.controls[a];
+      const field2 = formGroup.controls[b];
+
+      if (field1.value !== field2.value) {
+        field2.setErrors({ formGroupValidationFunction: true });
+      }
+    };
   }
   Register(): void {
     this.service.UserRegister(this.newUser.value).subscribe(data => {
