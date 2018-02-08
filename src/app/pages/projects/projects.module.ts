88714b8c8e157744fe9a7d3775e92874e5f29dde
ChristianBondesen <egeberg.simon@gmail.com>
2018-02-08 15:48:39 +0100

    adde mata

--- src/app/pages/projects/projects.module.ts
+++ src/app/pages/projects/projects.module.ts
@@ -5,9 +5,10 @@ import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectComponent } from './project/project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectService } from './project.service';
+import { MaterialModule } from '../../material.module';
 
 @NgModule({
-  imports: [CommonModule, ProjectsRoutingModule],
+  imports: [CommonModule, ProjectsRoutingModule, MaterialModule],
   declarations: [ProjectComponent, ProjectListComponent]
 })
 export class ProjectsModule {}
